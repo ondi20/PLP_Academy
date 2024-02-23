f470dc7b59fef6bc21655b7cc51ae888631f64be
Onyango Ondigo <138160676+ondi20@users.noreply.github.com>
2024-02-23 21:04:20 +0000

    first py script

---
+++ Python-Programming/Week1/user_input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Prompting the user for their name, age, and location
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+
+#Printing out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
